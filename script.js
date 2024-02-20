60fd22c261991ee42053bc52e600f1086352d80a
Mark Chweya <chweyamark@gmail.com>
2024-02-20 23:42:29 +0300

    Done!

---
+++ script.js
@@ -0,0 +1,11 @@
+$(function() {
+    $(".container").mouseenter(function() {
+        $(".card").stop().animate({
+            top: "-90px"
+        }, "slow");
+    }).mouseleave(function() {
+        $(".card").stop().animate({
+            top: 0
+        }, "slow");
+    });
+});
